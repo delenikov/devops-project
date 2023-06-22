725a377ccfe01510e1cee77e27ab01c112dc7d27
Stojan Delenikov <delenikov.stojan@gmail.com>
2023-06-23 00:29:56 +0200

    Added k8s files

--- devops-frontend/src/axios/axios.js
+++ devops-frontend/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'backend-service',
+    baseURL: 'http://localhost:8081',
     headers: {
         'Access-Control-Allow-Origin': '*',
     }
